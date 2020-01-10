1a7f2a926abd4dfe2b0f17b2f7a7b677fd808468
Andrei Ciucur <59577454+andrei-ccr@users.noreply.github.com>
2020-01-10 23:55:31 +0200

    Add script

---
+++ win7-rearmer.py
@@ -0,0 +1,34 @@
+import winreg
+import ctypes #For Admin rights check
+import subprocess #For running commands in CMD
+
+def is_admin():
+	try:
+		return ctypes.windll.shell32.IsUserAnAdmin()
+	except:
+		return False
+
+def fix_skip_rearm():
+	handle = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\SoftwareProtectionPlatform", 0, winreg.KEY_ALL_ACCESS)
+	val = winreg.QueryValueEx(handle, "SkipRearm")
+	if val==(0,4):
+		print("Increasing max rearms... ")
+		try:
+			winreg.SetValueEx(handle, "SkipRearm", 0, winreg.REG_DWORD, 1)
+			winreg.CloseKey(handle)
+			return True
+		except:
+			winreg.CloseKey(handle)
+			return False
+	else:
+		winreg.CloseKey(handle)
+		return True
+
+if is_admin():
+	res = fix_skip_rearm()
+	if res:
+		print("Rearming...  ", end="")
+		subprocess.check_output("SLMGR /REARM", shell=True)
+		print("DONE\n")
+else:
+	print("Run the script as Admin!")
